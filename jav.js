df87b992b7d147025a54d1b7c0e7d2dfca80ced8
Awosanya Oluwasegun <seguncotagmed@gmail.com>
2018-07-30 00:21:34 -0700

    CJT first code

---
+++ jav.js
@@ -0,0 +1,19 @@
+var Name = prompt("What is your Name");
+alert("welcome Mr "+ Name);
+alert("are you ready to play the Number game?");
+alert("alright then Hop on");
+var a = prompt("In put first number");
+var b = prompt("In put second number");
+var message = (Number(a) > Number(b))?"First is larger than " + b :" No second is larger than " + a;
+alert(message);
+document.write(message);
+alert("Thank you " + Name);
+
+
+ 
+ 
+ 
+
+
+
+
